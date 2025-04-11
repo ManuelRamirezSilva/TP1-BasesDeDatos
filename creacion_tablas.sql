5c5c3b8d182ac14f3c503771a12b0e0782d74db0
Lucio Luque Materazzi <lluquematerazzi@udesa.edu.ar>
2025-04-11 14:41:55 -0300

    cracion tablas final

--- creacion_tablas.sql
+++ creacion_tablas.sql
@@ -6,68 +6,59 @@ DROP TABLE IF EXISTS ZonasDelParque;
 DROP TABLE IF EXISTS Incidentes;
 DROP TABLE IF EXISTS Victimas;
 
-CREATE TABLE Dinosaurios(
-    id_dinosaurio INTEGER primary key NOT NULL,
-    fecha_nacimiento DATE NOT NULL,
-    especie VARCHAR(100) NOT NULL,
-    genero VARCHAR(100) NOT NULL,
-    altura FLOAT(2) NOT NULL,
-    peso FLOAT(2) NOT NULL
-);
-
 CREATE TABLE ZonasDelParque(
-    id_zona INTEGER primary key NOT NULL,
-    epoca VARCHAR(100) NOT NULL,
-    CHECK (epoca IN ('Triásico', 'Jurásico', 'Cretácico'))
+    id_zona INTEGER PRIMARY KEY NOT NULL CHECK (id_zona >= 0),
+    epoca VARCHAR(100) NOT NULL CHECK (epoca IN ('Triásico', 'Jurásico', 'Cretácico'))
 );
 
 CREATE TABLE Recintos(
-    id_recinto INTEGER primary key NOT NULL,
+    id_recinto INTEGER PRIMARY KEY NOT NULL,
     id_zona INTEGER NOT null,
     FOREIGN KEY (id_zona) REFERENCES ZonasDelParque(id_zona),
     area_m2 INTEGER NOT NULL,
-    tipo VARCHAR(100) NOT NULL CHECK IN ('Carnívoro', 'Herbívoro'),
-    estado VARCHAR(100) NOT NULL CHECK IN ('Óptimo', 'Subóptimo', 'Comprometido', 'Peligroso')
-    -- CHECK (tipo IN ('Carnívoro', 'Herbívoro')),
-    -- CHECK (estado IN ('Óptimo', 'Subóptimo', 'Comprometido', 'Peligroso'))
+    tipo VARCHAR(100) NOT NULL CHECK (tipo IN ('Carnívoro', 'Herbívoro')),
+    estado VARCHAR(100) NOT NULL CHECK (estado IN ('Óptimo', 'Subóptimo', 'Comprometido', 'Peligroso'))
 );
 
-
-CREATE TABLE Incidentes(
-    id_incidente INTEGER primary key NOT NULL,
-    tipo VARCHAR(100) NOT null,
-    fecha DATE NOT NULL,
-    hora TIME NOT NULL
-);
-
-CREATE TABLE Victimas(
-    dni INTEGER primary key NOT NULL,
-    tipo VARCHAR(100) NOT null,
-    nombre VARCHAR(100) NOT NULL,
-    herida VARCHAR(100) NOT NULL,
-    CHECK (tipo IN ('Visitante', 'Empleado', 'CEO'))
+CREATE TABLE Dinosaurios(
+    id_dinosaurio INTEGER PRIMARY KEY NOT NULL CHECK (id_dinosaurio >= 0),
+    fecha_nacimiento DATE NOT NULL,
+    especie VARCHAR(100) NOT NULL,
+    genero VARCHAR(100) NOT NULL,
+    altura FLOAT(2) NOT NULL CHECK (altura > 0),
+    peso FLOAT(2) NOT NULL CHECK (peso > 0)
 );
 
 CREATE TABLE VivenEn(
-    id_dinosaurio INTEGER primary key NOT NULL,
-    id_recinto INTEGER NOT null,
-    id_zona INTEGER NOT null,
+    id_dinosaurio INTEGER PRIMARY KEY NOT NULL CHECK (id_dinosaurio >= 0),
+    id_recinto INTEGER NOT NULL CHECK (id_recinto >= 0),
+    id_zona INTEGER NOT NULL CHECK (id_zona >= 0),
     FOREIGN KEY (id_dinosaurio) REFERENCES Dinosaurios(id_dinosaurio),
     FOREIGN KEY (id_recinto) REFERENCES Recintos(id_recinto),
     FOREIGN KEY (id_zona) REFERENCES ZonasDelParque(id_zona)
 );
 
+CREATE TABLE Victimas(
+    dni INTEGER PRIMARY KEY NOT NULL CHECK (dni >= 0),
+    tipo VARCHAR(100) NOT NULL CHECK (tipo IN ('Visitante', 'Empleado', 'CEO')),
+    nombre VARCHAR(100) NOT NULL,
+    herida VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE Incidentes(
+    id_incidente INTEGER PRIMARY KEY NOT NULL CHECK (id_incidente >= 0),
+    tipo VARCHAR(100) NOT null,
+    fecha DATE NOT NULL,
+    hora TIME NOT NULL,
+    CHECK (TIMESTAMP(fecha, hora) <= CURRENT_TIMESTAMP)
+);
+
 CREATE TABLE Involucrados(
-    id_dinosaurio INTEGER NOT NULL,
-    dni INTEGER NOT null,
-    id_incidente INTEGER NOT null,
+    id_dinosaurio INTEGER NOT NULL CHECK (id_dinosaurio >= 0),
+    dni INTEGER NOT NULL CHECK (dni >= 0),
+    id_incidente INTEGER NOT NULL CHECK (id_incidente >= 0),
     PRIMARY KEY (id_dinosaurio, dni, id_incidente),
     FOREIGN KEY (id_dinosaurio) REFERENCES Dinosaurios(id_dinosaurio),
     FOREIGN KEY (dni) REFERENCES Victimas(dni),
     FOREIGN KEY (id_incidente) REFERENCES Incidentes(id_incidente)
 );
-
-
-
-
-
